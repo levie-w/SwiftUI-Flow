24608bc5c7cad7bfe98702bdd961e441de6182c3
汪励为 <levie@wangliweideMacBook-Pro.local>
2024-03-15 15:47:25 +0800

    lowering target platform to iOS 14

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Flow",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v14),
         .macOS(.v12),
         .tvOS(.v15),
         .watchOS(.v8)
